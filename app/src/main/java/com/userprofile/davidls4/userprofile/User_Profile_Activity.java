64680c27395a02f01c9911fbd9755bbdd0121ea8
DavidTheMaaster <davidthegaamer@gmail.com>
2018-10-09 15:30:06 +0200

    Added json

--- app/src/main/java/com/userprofile/davidls4/userprofile/User_Profile_Activity.java
+++ app/src/main/java/com/userprofile/davidls4/userprofile/User_Profile_Activity.java
@@ -1,5 +1,6 @@
 package com.userprofile.davidls4.userprofile;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -13,13 +14,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-
 import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class User_Profile_Activity{
+public class User_Profile_Activity extends AppCompatActivity {
 
     private User_Profile user;
     private Gson gson;
@@ -29,11 +30,13 @@ public class User_Profile_Activity{
     private TextView followers;
     private TextView about_me;
     private ImageView user_image;
+    private RequestQueue queue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie);
+        setContentView(R.layout.activity_user__profile);
 
         gson = new Gson();
         queue = Volley.newRequestQueue(this);
@@ -43,21 +46,28 @@ public class User_Profile_Activity{
         following = findViewById(R.id.following);
         followers = findViewById(R.id.followers);
         about_me = findViewById(R.id.about_me);
+        user_image = findViewById(R.id.user_image);
 
         StringRequest req = new StringRequest(
                 Request.Method.GET,
-                "https://www.omdbapi.com/?apikey=80879df5&i=tt0317705", new Response.Listener<String>() {
+                "https://www.omdbapi.com/?apikey=80879df5&i=tt0486592", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 user = gson.fromJson(response, User_Profile.class);
                 updateUser();
-                Glide.with(User_Profile_Activity.this)
-                        .load(user.getUserImage())
-                        .into(user_image);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(User_Profile_Activity.this, "Error de xarxa", Toast.LENGTH_SHORT).show();
             }
         });
 
         queue.add(req);
+
+        Glide.with(this)
+                .load("file:///android_asset/lord.jpg")
+                .into(user_image);
     }
 
     private void updateUser() {
@@ -66,6 +76,6 @@ public class User_Profile_Activity{
         following.setText(user.getFollower());
         followers.setText(user.getFollowingNumber());
         about_me.setText(user.getAboutMe());
-
     }
 }
+
